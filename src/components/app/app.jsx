b3ff25c7379192872b34e5d856c915f716ce6e3e
d.kharo <d.kharo@contact-sys.com>
2019-11-05 18:39:49 +0300

    Личный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import MainScreen from '../main/main.jsx';
 
 const App = () => {
-  return <MainScreen/>;
+  const properties = [
+    {
+      id: `prop-1`,
+      caption: `Beautiful & luxurious apartment at great location`,
+    },
+    {
+      id: `prop-2`,
+      caption: `Wood and stone place`,
+    },
+    {
+      id: `prop-3`,
+      caption: `Canal View Prinsengracht`,
+    },
+    {
+      id: `prop-4`,
+      caption: `Nice, cozy, warm big bed apartment`,
+    },
+  ];
+  return <MainScreen properties={properties} />;
 };
 
 export default App;
