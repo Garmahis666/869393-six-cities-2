819759db1899afa8e9f45f5d1b24a281d63d83ac
Garmahis666 <garmahis@garmahis.ru>
2019-11-06 20:26:01 +0300

    Личный проект: под покровом тестов

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -11,18 +11,6 @@ it(`App is correctly rendered after relaunch`, () => {
       id: `prop-1`,
       caption: `Beautiful & luxurious apartment at great location`,
     },
-    {
-      id: `prop-2`,
-      caption: `Wood and stone place`,
-    },
-    {
-      id: `prop-3`,
-      caption: `Canal View Prinsengracht`,
-    },
-    {
-      id: `prop-5`,
-      caption: `Nice, cozy, warm big bed apartment`,
-    },
   ];
 
   const onClick = jest.fn();
