fa271aeff3fd4ba118bf09e811eb15d69a2ae7f2
Garmahis666 <garmahis@garmahis.ru>
2019-11-08 16:38:37 +0300

    Личный проект: под покровом тестов

---
+++ src/components/place-card/place-card.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PlaceCard from './place-card';
+
+it(`Main correctly renders after relaunch`, () => {
+  const testCard = {
+    id: `prop-1`,
+    caption: `Beautiful & luxurious apartment at great location`,
+    imgSrc: `img/apartment-01.jpg`,
+    type: `Aparment`,
+    priceCurrency: `€`,
+    priceValue: 120,
+    priceText: `night`,
+  };
+
+  const onMouseOver = () => {};
+
+  const tree = renderer
+    .create(<PlaceCard key={testCard.id} placeCardInfo={testCard} onMouseOver={onMouseOver} />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
--- src/components/places-list/places-list.test.js
+++ src/components/places-list/places-list.test.js
@@ -1,25 +1,22 @@
-
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Main from './main';
+import PlacesList from './places-list';
 
 it(`Main correctly renders after relaunch`, () => {
   const testCard = [
     {
-      id: `prop-2`,
-      caption: `Wood and stone place`,
-      imgSrc: `img/room.jpg`,
-      type: `Private room`,
+      id: `prop-1`,
+      caption: `Beautiful & luxurious apartment at great location`,
+      imgSrc: `img/apartment-01.jpg`,
+      type: `Aparment`,
       priceCurrency: `€`,
-      priceValue: 80,
+      priceValue: 120,
       priceText: `night`,
     },
   ];
 
   const tree = renderer
-    .create(<Main
-      placeCardsInfo={testCard}
-    />)
+    .create(<PlacesList key="PlacesList" placeCardsInfo={testCard} />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
